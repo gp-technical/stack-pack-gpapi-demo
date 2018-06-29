946dd6906018680f840369880799e5778a10e2f3
biofractal <biofractal@gmail.com>
2018-06-29 14:22:43 +0100

    improved clarity of facade urls

--- src/express.js
+++ src/express.js
@@ -19,47 +19,47 @@ const start = async () => {
       res.send(`The ${process.env.API_NAME} api is running : ${new Date().toLocaleString('en-GB')}`)
     })
 
-    app.get('/gp/branding/:id', async (req, res) => {
+    app.get('/branding/:id', async (req, res) => {
       res.json(await gpapi.get(`branding/${req.params.id}`))
     })
 
-    app.get('/gp/subscription/:url', async (req, res) => {
+    app.get('/subscription/:url', async (req, res) => {
       res.json(await gpapi.get(`subscription/url/${req.params.url}`))
     })
 
-    app.get('/gp/:contentmodelid/primary-folders', async (req, res) => {
+    app.get('/:contentmodelid/primary-folders', async (req, res) => {
       res.json(await gpapi.get(`location/contentmodel/${req.params.contentmodelid}/primary-folders`))
     })
 
-    app.get('/gp/folder/:folderId', async (req, res) => {
+    app.get('/folder/:folderId', async (req, res) => {
       res.json(await gpapi.get(`location/parent-folder/${req.params.folderId}/child-folders`))
     })
 
-    app.get('/gp/:folderId/items', async (req, res) => {
+    app.get('/:folderId/items', async (req, res) => {
       res.json(await gpapi.get(`folder/${req.params.folderId}/items/simple`))
     })
 
-    app.get('/gp/content/:type/:id', async (req, res) => {
+    app.get('/content/:type/:id', async (req, res) => {
       res.json(await gpapi.get(`contentitem/${req.params.type}/${req.params.id}`))
     })
 
-    app.get('/gp/:subscriptionId/content/:type/:id', async (req, res) => {
+    app.get('/:subscriptionId/content/:type/:id', async (req, res) => {
       res.json(await gpapi.get(`contentitem/${req.params.subscriptionId}/${req.params.type}/${req.params.id}`))
     })
 
-    app.get('/gp/contentmodel/:id/topical', async (req, res) => {
+    app.get('/contentmodel/:id/topical', async (req, res) => {
       res.json(await gpapi.get(`contentmodel/${req.params.id}/most-read`))
     })
 
-    app.get('/gp/contentmodel/:id/most-read', async (req, res) => {
+    app.get('/contentmodel/:id/most-read', async (req, res) => {
       res.json(await gpapi.get(`contentmodel/${req.params.id}/most-read`))
     })
 
-    app.get('/gp/contentmodel/:id/most-recommended', async (req, res) => {
+    app.get('/contentmodel/:id/most-recommended', async (req, res) => {
       res.json(await gpapi.get(`contentmodel/${req.params.id}/most-recommended`))
     })
 
-    app.get('/gp/contentmodel/:id/search', async (req, res) => {
+    app.get('/contentmodel/:id/search', async (req, res) => {
       res.json(await gpapi.get(`contentmodel/${req.params.id}/search?terms=${req.query.terms}`))
     })
 
